76d559d803667a675d864550b3ea3cdad22b416d
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-08 15:36:48 +0100

    3 files modified

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ console.log(value);
 console.log(wysokosc);
 k1.style.top = wysokosc - value * 0.3  +'px';
 k2.style.top = value * 2 + 'px';
-k2.style.opacity = 100 - value * 0.3 + '%';
-k3.style.top = wysokosc - value + 'px';
+
+k3.style.top = wysokosc / (value +1) * 100 + 'vh';
 
 
 
