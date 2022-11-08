f5c3df1176de1ccd82e7073fca8ffb23a5f34344
JZSANECKI <115653252+JZSANECKI@users.noreply.github.com>
2022-11-08 18:22:23 +0100

    2 files modified

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let wysokosc = window.innerHeight;
 let szerokosc = window.innerWidth;
 window.addEventListener('scroll', function() {
 let value = window.scrollY;
-console.log(value);
-console.log(wysokosc);
+
 k1.style.top = wysokosc - value *0.7 + 'px';
 k2.style.top = value * 2 + 'px';
 
-k3.style.top = wysokosc / (value +1) * 100 - 30 + 'vh';
-
+k3.style.top = wysokosc / (value +1) * 100 + 50 + 'vh';
+console.log(k3.style.top);
+console.log(value);
+console.log(wysokosc);
 
 
 
